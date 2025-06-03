1a3eb7cd2b583c04f453bbff72c96ed6981a0610
Sergi <ssegarragarcia@gmail.com>
2025-06-03 20:18:37 +0000

    mvp ready for production

---
+++ revlo-directory/src/components/ui/login-form.jsx
@@ -0,0 +1,84 @@
+"use client"
+
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { useState } from "react"
+import { supabase } from "@/lib/supabaseClient"
+import { useRouter } from "next/navigation"
+import { toast } from "sonner"
+import Navbar from "./navbar"
+
+export function LoginForm({className, ...props}) {
+  const router = useRouter()
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleLogin = async (e) => {
+   e.preventDefault()
+   const {error} = await supabase.auth.signInWithPassword({email, password})
+   if(error) {
+    toast.error("Login failed")
+   }
+   else {
+    toast.success("Logged in succesfully")
+    setTimeout(() => router.push("/"), 1500)
+   }
+  }
+
+  return (
+    <>
+    <Navbar showSearchbar={false}/>
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader>
+          <CardTitle>Login to your account</CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={handleLogin}>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-3">
+                <Label htmlFor="email">Email</Label>
+                <Input id="email" value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="m@example.com" required />
+              </div>
+              <div className="grid gap-3">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                  <a
+                  href="/update-password"
+                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline">
+                    Update your password?
+                  </a>
+                </div>
+                <Input id="password" value={password} onChange={(e) => setPassword(e.target.value)} type="password" required />
+              </div>
+              <div className="flex flex-col gap-3">
+                <Button type="submit" className="w-full">
+                  Login
+                </Button>
+              </div>
+            </div>
+            <div className="mt-4 text-center text-sm">
+              Don&apos;t have an account?{" "}
+              <a href="/signup" className="underline underline-offset-4">
+                Sign up
+              </a>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+    </>
+  );
+}
